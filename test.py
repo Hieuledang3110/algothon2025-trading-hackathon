06e256db42bce6448d3c0c7f229b44ada2aa35c0
Eric Wu <cwuu0090@student.monash.edu>
2025-06-22 19:06:36 +1000

    Added feedback_eval
    feedback_eval now shows charts of positions and PL and stock prices.
    
    Also refined the parameters of the basic greedy algorithm. At this point it's definitely overfitting though.

--- test.py
+++ test.py
@@ -13,14 +13,14 @@ averages = np.mean(data, axis = 0)
 temp = data # want the median to be the middle stock so im making it odd
 medians = np.median(temp, axis = 0)
 
-for i in range(20,40):
-    plt.plot(days,data[i])
+# for i in range(20,40):
+#     plt.plot(days,data[i])
 
 
-plt.plot(days,averages, color="red", linewidth=5)
-plt.plot(days,medians, color="black", linewidth=5)
+# plt.plot(days,averages, color="red", linewidth=5)
+# plt.plot(days,medians, color="black", linewidth=5)
 
-plt.xlim(0,100)
+# plt.xlim(0,100)
 # plt.show()
 
 
@@ -34,13 +34,13 @@ def getMyPosition(data):
     for prices in data:
         iLast = len(prices)
 
+        longTermLength = 500
         midTermLength = 89
         shortTermLength = 29
 
         upTrendTolerance = 1.05
-        downTrendTolerance = 0.941
+        downTrendTolerance = 0.83
         
-
         #Getting the average price during the last shotTermLength days
         if iLast < shortTermLength:
             recentPrices = data
@@ -57,8 +57,11 @@ def getMyPosition(data):
         buyFactor = 0
 
         #By this definition, something is 'trending up' if their stock is up since midTermLength days ago :skull: 
+        #This is unused and will probably stay unused. It seems pretty useless in its current form
         isTrendingUp = False
         isTrendingDown = False
+        isLongTrendingUp = False
+        isLongTrendingDown = False
 
         if iLast < midTermLength:
             historicalPrice = prices[0]
@@ -68,22 +71,43 @@ def getMyPosition(data):
             isTrendingDown = True
         elif currentPrice > historicalPrice*upTrendTolerance:
             isTrendingUp = True
+
+        if iLast < longTermLength:
+            historicalPrice = prices[0]
+        else:
+            historicalPrice = prices[-longTermLength]
+        if currentPrice < historicalPrice*downTrendTolerance:
+            isLongTrendingDown = True
+        elif currentPrice > historicalPrice*upTrendTolerance:
+            isLongTrendingUp = True
+
+
+
             
         #Checking how far away the current price is from the short term average
         devianceFactor = (currentPrice-averagePrice)/maxVariation
         devianceFactor = max(min(devianceFactor,1),-1)  #clamp between -1,1
 
-        #It seems that full sending when buying is the most profitable bruh. This might be partially due to the savings from comissions
-        if (devianceFactor > 0.329):
+
+        #It seems that full sending when buying is the most profitable bruh.
+        if (devianceFactor > 0.329):   
             if (devianceFactor < 0.541):
                 buyFactor = (devianceFactor+1)/0.54
                 buyFactor = 1
-        #It seems that being cautious with shorting is important, possibly because potential for downside is greater than potential for upside
-        #Here, we short if the stock is moderately down AND is down since midTermLength days ago.
-        if (devianceFactor < -0.145 and isTrendingDown):
-            if (devianceFactor > -0.52):
-                buyFactor = -(devianceFactor-0.38)/0.52
-        
+
+        #Upper and lower bounds for the devianceFactor to determine when to buy
+        a = -1
+        b = -0.145
+        fullSendThreshold = 0
+        tightnessPower = 3
+        #It seems that just full send buying when the stock is down is also profitable bruh. The fancy formula here is completely uselss
+        #This is probably where machine learning is needed
+        if (devianceFactor < b):
+            if (devianceFactor > a):
+                c = 1/(b+1)
+                buyFactor = -(c*(devianceFactor-fullSendThreshold))**tightnessPower + 1
+
+
         buyAmount = maxDollarPosition*buyFactor
         sharePosition = buyAmount/currentPrice
 
@@ -91,6 +115,3 @@ def getMyPosition(data):
         final_positions = np.array([int(pos) for pos in positions])
     
     return positions
-
-newData = np.loadtxt("prices.txt")
-print(getMyPosition(newData))   
