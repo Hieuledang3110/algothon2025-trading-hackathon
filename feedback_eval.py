6a2ab60eb7d9d4c1a635d6f566b895b85e15101f
Eric Wu <cwuu0090@student.monash.edu>
2025-06-24 05:58:13 +1000

    Temp Push
    Broken ass shit

--- feedback_eval.py
+++ feedback_eval.py
@@ -4,14 +4,17 @@
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
-from test import getMyPosition as getPosition
+from test import breakoutStrategy as getPosition
+from test import SMA
+from test import WMA
+from sklearn.linear_model import LinearRegression
 
 ##FEEDBACK VARIABLES
 
 #Set if you want to look at the chart
 openChart = True
 #Opens up the graphs related to this stock
-stockIndex = 46
+stockIndex = 12
 #Change this to see a stock's price before the trading period begins
 headStart = 500
 #The amount of biggest losers/winners you want to see
@@ -24,6 +27,7 @@ nt = 0
 commRate = 0.0005
 dlrPosLimit = 10000
 
+
 def loadPrices(fn):
     global nt, nInst
     df=pd.read_csv(fn, sep='\s+', header=None, index_col=None)
@@ -33,6 +37,21 @@ def loadPrices(fn):
 pricesFile= "./prices.txt"   # "./priceSlice_test.txt"
 prcAll = loadPrices(pricesFile)
 print ("Loaded %d instruments for %d days" % (nInst, nt))
+
+def smooth_trend_regression(values, long_window=200, smooth_window=20):
+    # Step 1: Get long-term trend using all data
+    X_all = np.arange(len(values)).reshape(-1, 1)
+    long_trend = LinearRegression().fit(X_all, values)
+    
+    # Step 2: Smooth recent residuals
+    trend_line = long_trend.predict(X_all)
+    residuals = values - trend_line
+    recent_residuals = residuals[-smooth_window:]
+    smoothed_residual = np.mean(recent_residuals)
+    
+    # Combine: long trend + smoothed recent deviation
+    next_trend = long_trend.predict([[len(values)]])[0]
+    return next_trend + smoothed_residual
 
 def calcPL(prcHist, numTestDays,stockIndex,headStart,nStocksFeedback):
     cash = 0
@@ -71,6 +90,7 @@ def calcPL(prcHist, numTestDays,stockIndex,headStart,nStocksFeedback):
 
             #Temp addition to track position history
             for i in range(len(prcHist)):
+                if t == 743:
                 positionHistory[i].append(newPosOrig[i])
                 DailyCashHistory[i].append(float(DailyCashHistory[i][-1] - curPrices[i]*deltaPos[i] + dvolumes[i]*commRate))
 
@@ -101,15 +121,70 @@ def calcPL(prcHist, numTestDays,stockIndex,headStart,nStocksFeedback):
         positionHistory[stockIndex][i] *= (prcHist[stockIndex][startDay]/10000)
         positionHistory[stockIndex][i] *= float(prcHist[stockIndex][startDay])
 
+    #DONT FORGET TO CHANGE THIS
+    movingAverageLength1 = 30
+    SMAs = [[] for _ in range(len(prcHist))]
+    EMAs = [[] for _ in range(len(prcHist))]
+    for i in range(len(prcHist)):
+        stockPrices = prcHist[i]
+        for j in range(movingAverageLength1,len(stockPrices)):
+            SMAs[i].append(SMA(movingAverageLength1,stockPrices[0:j+1]))
+            EMAs[i].append(WMA(movingAverageLength1,stockPrices[0:j+1]))
+
+
+    data = np.loadtxt("prices.txt")
+    data = np.rot90(data)
+    print(data)
+    stock_index = 12
+    prices = data[stockIndex]
+
+    # Calculate predictions from day 150 onwards
+    predictions = []
+    upper_bounds = []
+    lower_bounds = []
+    actual_days = []
+
+    for day in range(150, len(prices)):
+        current_data = prices[:day]
+        prediction = smooth_trend_regression(current_data, long_window=200, smooth_window=20)
+        
+        # Calculate bounds
+        percentage = 0.045
+        upper_bound = prediction * (1 + percentage)
+        lower_bound = prediction * (1 - percentage)
+        
+        predictions.append(prediction)
+        upper_bounds.append(upper_bound)
+        lower_bounds.append(lower_bound)
+        actual_days.append(day)
+
+    # Plot
+    plt.figure(figsize=(12, 6))
+    plt.plot(range(len(prices)), prices, 'b-', label='Actual Prices')
+    plt.plot(actual_days, predictions, 'ro-', label='Predictions', markersize=3)
+    plt.plot(actual_days, upper_bounds, 'g--', label='Upper Bound')
+    plt.plot(actual_days, lower_bounds, 'g--', label='Lower Bound')
+    plt.fill_between(actual_days, lower_bounds, upper_bounds, alpha=0.2, color='green')
+
+    plt.plot([i for i in range(startDay+1, nt+1)],positionHistory[stockIndex],color="orange", linewidth=1)
+
+    plt.title(f'Stock {stockIndex} - Regression Predictions')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+
+    '''
     plt.figure(1)
     plt.title('Black = Stock Price, Orange = Position (scaled so that the max position is roughly equal to the average price of the stock)')
     plt.plot([i for i in range(startDay+1, nt+1)],positionHistory[stockIndex],color="orange", linewidth=1)
-    plt.plot([i for i in range(startDay+1-headStart, nt+1)],prcHist[stockIndex,range(startDay-headStart, nt)],color="black", linewidth=1)
+    plt.plot([i for i in range(max(startDay+1-headStart,0), nt)],prcHist[stockIndex,range(max(startDay+1-headStart,0),nt)],color="black", linewidth=1)
+    
     plt.figure(2)
     plt.title('Green = Total Earnings, Red = Daily Earnings)')
     plt.plot([i for i in range(startDay+1, nt+1)],DailyPLHistory[stockIndex][:-1],color="red", linewidth=1)
     plt.plot([i for i in range(startDay+1, nt+1)],TotalPLHistory[stockIndex][:-2],color="green", linewidth=2)
     plt.plot([i for i in range(startDay+1, nt+1)],[0 for _ in range(startDay+1, nt+1)],color="grey", linewidth=1)
+    '''
 
     biggestWinnersIndices = []
     biggestLosersIndices = []
@@ -140,6 +215,7 @@ def calcPL(prcHist, numTestDays,stockIndex,headStart,nStocksFeedback):
     if (plstd > 0):
         annSharpe = np.sqrt(249) * plmu / plstd
     return (plmu, ret, plstd, annSharpe, totDVolume)
+
 
 # Calculate test period (80% to 100% of data)
 total_days = nt
--- test.py
+++ test.py
@@ -1,31 +1,141 @@
 #Eric is using this for testing
-
+ 
 from matplotlib import pyplot as plt
-import pandas 
+import pandas as pd
 import numpy as np
-
-data = np.loadtxt("prices.txt")
-data = np.rot90(data)
-days = list(range(1,len(data[0])+1))
-
-#average/median price of all stocks on a day
-averages = np.mean(data, axis = 0)
-temp = data # want the median to be the middle stock so im making it odd
-medians = np.median(temp, axis = 0)
-
-# for i in range(20,40):
-#     plt.plot(days,data[i])
+from scipy.stats import pearsonr
+from sklearn.linear_model import LinearRegression
 
 
-# plt.plot(days,averages, color="red", linewidth=5)
-# plt.plot(days,medians, color="black", linewidth=5)
+def SMA(timePeriod, stockPrices):
+    if timePeriod > len(stockPrices)+1:
+        raise ValueError
+    
+    priceSum = 0
+    for price in stockPrices[len(stockPrices)-timePeriod:len(stockPrices)]:
+        priceSum += price
 
-# plt.xlim(0,100)
-# plt.show()
+    movingAverage = priceSum/timePeriod
+    return movingAverage
 
+def WMA(timePeriod, stockPrices):
+    if timePeriod > len(stockPrices)+1:
+        print(f'Error: WMA function was given a timePeriod of {timePeriod}, but the stock price history was only {len(stockPrices)+1} elements long')
+        raise ValueError
+    
+    priceSum = 0
+    weight = 1
+    for i in range(len(stockPrices)-timePeriod,len(stockPrices)):
+        priceSum += stockPrices[i]*weight
+        weight += 1
+
+    movingAverage = (priceSum*2)/(timePeriod*(timePeriod+1))
+    return movingAverage
+
+###Gotta do this
+def EMA(stockPrices):
+    stockValues = pd.DataFrame({'Stock_Values': stockPrices})
+    result = stockValues.ewm(com=2).mean()
+    numpy_array = result.to_numpy()
+    return numpy_array
+
+def getReturns(data):
+    stockReturns = [[] for _ in range(len(data))]
+    for i in range(len(data)):
+        stockPrices = data[i]
+        for j in range(1,len(stockPrices)):
+            stockReturns[i].append((stockPrices[j]-stockPrices[j-1])/stockPrices[j-1])
+    return stockReturns
+
+def getCorrelations(data,maxLag):
+
+    correlations = [[[] for _ in range(len(data))] for __ in range(2*maxLag+2)]
+    p_values = [[[] for _ in range(len(data))] for __ in range(2*maxLag+2)]
+
+    stockReturns = getReturns(data)
+
+    for lag in range(-maxLag,maxLag+1):
+        for i in range(len(stockReturns)):
+            stock1Returns = stockReturns[i]
+            for j in range(len(stockReturns)):
+                stock2Returns = stockReturns[j]
+
+                if lag > 0:  #Note: if lag is positive, stock2 lags behind stock1. Opposite otherwise.
+                    buffer = ["This should not be read" for _ in range(lag)]
+                    offsetStock2Returns = buffer + stock2Returns
+                    correlation, p_value = pearsonr(stock1Returns[abs(lag):], offsetStock2Returns[abs(lag):-lag])
+                elif lag < 0:
+                    buffer = ["This should not be read" for _ in range(-lag)]
+                    offsetStock1Returns = buffer + stock1Returns
+                    correlation, p_value = pearsonr(offsetStock1Returns[abs(lag):lag], stock2Returns[abs(lag):])
+                elif lag == 0:
+                    correlation, p_value = pearsonr(stock1Returns, stock2Returns)
+
+                lagIndex = maxLag + lag
+                correlations[lagIndex][i].append(correlation)
+                p_values[lagIndex][i].append(p_value)
+
+    return correlations, p_values   
+
+def linear_regression_single_array(values):
+    X = np.arange(len(values)).reshape(-1, 1)  # Time indices as X
+    y = np.array(values)
+    
+    model = LinearRegression().fit(X, y)
+    slope = model.coef_[0]
+    intercept = model.intercept_
+    
+    # Predict next value
+    next_value = model.predict([[len(values)]])[0]
+
+    return slope, intercept, next_value    
+
+
+def momentumStrategy(data):
+    maxDollarPosition = 10000
+    timeHorizon = 5
+    movingAverageLength = 30
+
+
+    positions = []
+
+    for prices in data:
+        WMAs = [0 for _ in range(timeHorizon)]
+        SMAs = [0 for _ in range(timeHorizon)]
+        buyFactor = 0
+
+        firstDay = len(prices)-timeHorizon
+        for i in range(len(WMAs)):
+            WMAs[i] = WMA(movingAverageLength,prices[:firstDay + i])
+        for i in range(len(SMAs)):
+            SMAs[i] = SMA(movingAverageLength,prices[:firstDay + i])
+
+        significantDeviation = False
+        for i in range(len(SMAs)):
+            difference = abs(SMAs[i]-WMAs[i])
+            average = (SMAs[i]+WMAs[i])/2
+            if difference/average > 0.006:
+                significantDeviation = True
+        
+        # if significantDeviation:
+        #     if WMAs[-1] > WMAs[-4]*1.004:
+        #         buyFactor = 1
+        #     elif WMAs[-1] < WMAs[-4]/1.004:
+        #         buyFactor = -1
+
+        if WMAs[-1] > SMAs[-1]*1.01:
+            buyFactor = 1
+        
+
+        
+        position = buyFactor*maxDollarPosition/prices[-1]
+        positions.append(position)
+    
+    return positions
 
 #Extremely simple mean reversion just to test things out. No shorting for now.
 def getMyPosition(data):
+
     # data = np.rot90(data)
     positions = []
 
@@ -93,7 +203,7 @@ def getMyPosition(data):
         if (devianceFactor > 0.329):   
             if (devianceFactor < 0.541):
                 buyFactor = (devianceFactor+1)/0.54
-                buyFactor = 1
+                buyFactor = 0
 
         #Upper and lower bounds for the devianceFactor to determine when to buy
         a = -1
@@ -115,3 +225,321 @@ def getMyPosition(data):
         final_positions = np.array([int(pos) for pos in positions])
     
     return positions
+
+
+def plotHeatmap(thisCor,title):
+    plt.figure(figsize=(8, 6))
+    plt.imshow(thisCor, cmap='coolwarm', vmin=-1, vmax=1)
+    plt.colorbar()
+    plt.title(title)
+
+
+def lagTrade(data):
+    maxDollarPosition = 10000
+    positions = [0 for _ in range(len(data))]
+
+    EMAs = [[] for _ in range(len(data))]
+    for i in range(len(EMAs)):
+        EMAs[i] = EMA(data[i])
+
+    correlations, p_values = getCorrelations(EMAs,1)
+    correlatedPairs = []
+
+    oneDayLagCorrelations = correlations[0]
+    oneDayLagP_Values = p_values[0]
+    for i in range(len(oneDayLagCorrelations)):
+        for j in range(len(oneDayLagCorrelations[i])):
+            if oneDayLagCorrelations[i][j] > 0.22:
+                if oneDayLagP_Values[i][j] < 0.01:
+                    correlatedPairs.append([i,j])
+    
+    stockReturns = getReturns(data)
+    lastReturns = []
+    for stock in stockReturns:
+        lastReturns.append(stock[-1])
+
+    for pair in correlatedPairs:
+        lagger = pair[0]
+        setter = pair[1]
+        if lastReturns[setter] > 0.01:
+            positions[lagger] = maxDollarPosition/data[lagger][-1]
+    
+    return positions
+
+
+def smooth_trend_regression(values, long_window=200, smooth_window=20):
+    # Step 1: Get long-term trend using all data
+    X_all = np.arange(len(values)).reshape(-1, 1)
+    long_trend = LinearRegression().fit(X_all, values)
+    
+    # Step 2: Smooth recent residuals
+    trend_line = long_trend.predict(X_all)
+    residuals = values - trend_line
+    recent_residuals = residuals[-smooth_window:]
+    smoothed_residual = np.mean(recent_residuals)
+    
+    # Combine: long trend + smoothed recent deviation
+    next_trend = long_trend.predict([[len(values)]])[0]
+    return next_trend + smoothed_residual
+
+
+def get_single_day_prediction(prices, target_day, long_window=200, smooth_window=20):
+    """Get prediction for just one specific day"""
+    if target_day >= len(prices):
+        return None
+    
+    # Use data up to target_day
+    current_data = prices[:target_day]
+    
+    # Apply the smooth trend regression
+    prediction = smooth_trend_regression(current_data, long_window, smooth_window)
+    
+    return prediction
+
+def breakoutStrategy(data):
+    positions = []
+    maxDollarPosition = 10000
+
+    for i in range(len(data)):
+        prices = data[i]
+        latestPrice = prices[-1]
+        buyFactor = 0
+
+        prediction = smooth_trend_regression(prices, long_window=200, smooth_window=20)
+
+        # Create upper and lower bounds
+        percentage = 0.045  # % bands
+        upper_bound = (prediction) * (1 + percentage)
+        lower_bound = (prediction) * (1 - percentage)
+        
+
+        if latestPrice < lower_bound:
+            buyFactor = -1
+        elif latestPrice > upper_bound:
+            buyFactor = 1
+
+        buyAmount = maxDollarPosition*buyFactor
+        sharePosition = buyAmount/latestPrice
+        positions.append(sharePosition)
+
+        print(i, prediction, upper_bound, lower_bound, latestPrice, buyFactor)
+
+    print(positions)
+    return positions  
+
+def breakoutStrategy2(prcSoFar):
+    """
+    Breakout strategy that buys when price exits above upper bound,
+    sells when price exits below lower bound
+   
+    Args:
+        prcSoFar: numpy array of shape (nInst, nDays) with price history
+       
+    Returns:
+        numpy array of positions for each instrument
+    """
+    nInst, nDays = prcSoFar.shape
+    positions = []
+
+    print(nInst)
+   
+    for i in range(nInst):
+        prices = prcSoFar[i, :]
+    
+
+        # Get current prediction and bounds
+        current_prediction = smooth_trend_regression(prices, long_window=200, smooth_window=20)
+           
+        # Calculate bounds (2% bands)
+        percentage = 0.02
+        upper_bound = current_prediction * (1 + percentage)
+        lower_bound = current_prediction * (1 - percentage)
+           
+        current_price = prices[-1]
+           
+        # Check for breakout (need at least 2 days)
+        if nDays >= 2:
+            # Get previous day's prediction and bounds
+            prev_prices = prices[:-1]
+            prev_prediction = smooth_trend_regression(prev_prices, long_window=200, smooth_window=20)
+            prev_upper = prev_prediction * (1 + percentage)
+            prev_lower = prev_prediction * (1 - percentage)
+            previous_price = prices[-2]
+               
+            # Detect breakouts
+            broke_above = (previous_price <= prev_upper) and (current_price > upper_bound)
+            broke_below = (previous_price >= prev_lower) and (current_price < lower_bound)
+               
+            # Position sizing
+            max_position_value = 10000 * 0.8  # 80% of limit for safety
+            shares_to_trade = int(max_position_value / current_price) if current_price > 0 else 0
+               
+            if broke_above:
+                positions.append(shares_to_trade)  # Buy
+            elif broke_below:
+                positions.append(shares_to_trade)  # Sell
+            else:
+                positions.append(0)  # No position
+
+    return positions
+
+data = np.loadtxt("prices.txt")
+data = np.rot90(data)
+
+print(data)
+days = list(range(1,len(data[0])+1))
+
+
+'''
+#average/median price of all stocks on a day
+averages = np.mean(data, axis = 0)
+temp = data # want the median to be the middle stock so im making it odd
+medians = np.median(temp, axis = 0)
+stockIndex = 4
+
+
+for i in range(1):
+    plt.plot(days,data[i])
+    plt.plot(days,EMA(data[i])) 
+plt.plot(days,averages, color="black", linewidth=5)
+
+# plt.plot(days,medians, color="red", linewidth=5)
+
+plt.show()
+'''
+
+
+instrument = 12
+prices = data[instrument]
+
+# Calculate predictions from day 150 onwards
+predictions1 = []
+upperBound1 = []
+lowerBound1 = []
+predictions2 = []
+upperBound2 = []
+lowerBound2 = []
+actual_days1 = []
+actual_days2 = []
+defaultFreedomFactor = 0.04
+maxExpansionFactor = 16
+maxDifferenceConsidered = 0.05
+
+
+# Add SMA
+sma_30 = []
+for i in range(29, len(prices)):  # Start from day 30 (index 29)
+    sma_30.append(np.mean(prices[i-29:i+1]))
+
+# Add EMA (e.g., 20-day EMA)
+ema_20 = pd.Series(prices).ewm(span=20).mean()
+
+
+# Or multiple EMAs for crossover strategies
+ema_12 = pd.Series(prices).ewm(span=12).mean()
+ema_4 = pd.Series(prices).ewm(span=4).mean()
+ema_26 = pd.Series(prices).ewm(span=26).mean()
+
+delta = pd.Series(prices).diff()
+gain = delta.where(delta > 0, 0).rolling(14).mean()
+loss = (-delta.where(delta < 0, 0)).rolling(14).mean()
+rs = gain / loss
+rsi = 100 - (100 / (1 + rs))
+
+
+for day in range(20, len(prices)):
+    # Use data up to current day to predict next day
+    current_data = prices[:day]
+    #slope, intercept, next_value1 = linear_regression_single_array(current_data[max(0,len(current_data)-300):])
+    slope, intercept, next_value1 = linear_regression_single_array(ema_4[max(0,day-20):day])
+    predictions1.append(next_value1)
+    difference1 = abs(sma_30[day-29]-ema_12[day])/prices[day]
+    difference2 = abs(next_value1-ema_12[day])/prices[day]
+    expansionFactor = max(difference1,difference2)*(defaultFreedomFactor)*(maxExpansionFactor)/maxDifferenceConsidered
+    #lowerBound.append(next_value + next_value*(defaultFreedomFactor+expansionFactor))
+    #upperBound.append(next_value - next_value*(defaultFreedomFactor+expansionFactor))
+    lowerBound1.append(next_value1 + next_value1*(defaultFreedomFactor))
+    upperBound1.append(next_value1 - next_value1*(defaultFreedomFactor))
+    actual_days1.append(day)
+
+
+# Plot
+plt.figure(figsize=(12, 6))
+plt.plot(range(len(prices)), prices, color='black', label='Actual Prices', alpha=0.7)
+#plt.plot(actual_days1, predictions1, 'r-', label='Daily Predictions 1', markersize=3)
+#plt.plot(actual_days1, upperBound1, 'b-', label='Upper Bound 1', markersize=2)
+#plt.plot(actual_days1, lowerBound1, 'b-', label='Lower Bound 1', markersize=2)
+#plt.plot(actual_days1, upperBound2, 'g-', label='Upper Bound 2', markersize=2)
+#plt.plot(actual_days1, lowerBound2, 'g-', label='Lower Bound 2', markersize=2)
+#plt.plot(range(29, len(prices)), sma_30, 'g-', label='30-day SMA', linewidth=2)
+#plt.plot(range(len(prices)), ema_20, 'orange', label='20-day EMA', linewidth=2)
+#plt.plot(range(len(prices)), ema_12, 'cyan', label='12-day EMA')
+plt.plot(range(len(prices)), ema_4, 'cyan', label='4-day EMA')
+#plt.plot(range(len(prices)), ema_26, 'magenta', label='26-day EMA')
+
+
+
+smooth_predictions = []
+actual_days = []
+
+for day in range(150, len(prices)):
+    current_data = prices[:day]
+    smooth_pred = smooth_trend_regression(current_data, long_window=200, smooth_window=20)
+    smooth_predictions.append(smooth_pred)
+    actual_days.append(day)
+
+# Create upper and lower bounds
+percentage = 0.045  # % bands
+upper_bound = np.array(smooth_predictions) * (1 + percentage)
+lower_bound = np.array(smooth_predictions) * (1 - percentage)
+
+# Plot everything
+plt.plot(range(len(prices)), prices, 'b-', label='Actual Prices', alpha=0.7)
+plt.plot(actual_days, smooth_predictions, 'ro-', label='Smooth Predictions', markersize=3)
+plt.plot(actual_days, upper_bound, 'g--', label='Upper Bound', alpha=0.7)
+plt.plot(actual_days, lower_bound, 'g--', label='Lower Bound', alpha=0.7)
+
+# Optional: Fill the area between bounds
+plt.fill_between(actual_days, lower_bound, upper_bound, alpha=0.2, color='green')
+
+
+plt.xlabel('Day')
+plt.ylabel('Price')
+plt.title(f'Daily Price Predictions from Day 75 - Instrument {instrument}')
+plt.legend()
+plt.grid(True)
+
+# Add to plot (usually plotted separately due to 0-100 scale)
+# plt.figure(1)
+# plt.figure(figsize=(12, 4))
+# plt.plot(range(len(prices)), rsi, 'red', label='RSI')
+# plt.axhline(70, color='gray', linestyle='--', alpha=0.7)  # Overbought
+# plt.axhline(30, color='gray', linestyle='--', alpha=0.7)  # Oversold
+# plt.ylabel('RSI')
+# plt.legend()
+
+plt.show()
+
+'''
+EMAs = [[] for _ in range(len(data))]
+for i in range(len(EMAs)):
+    EMAs[i] = EMA(data[i])
+
+maxLag = 5
+curLag = 5
+correlations, p_values = getCorrelations(EMAs,maxLag)
+
+count = 0
+
+plt.figure(figsize=(8, 6))
+plt.imshow(correlations[maxLag+curLag], cmap='coolwarm', vmin=-1, vmax=1)
+plt.colorbar()
+plt.title(f'Lag = {curLag}')
+
+curLag = 0
+plt.figure(1)
+plt.figure(figsize=(8, 6))
+plt.imshow(correlations[maxLag+curLag], cmap='coolwarm', vmin=-1, vmax=1)
+plt.colorbar()
+plt.title(f'Lag = {curLag}')
+'''
